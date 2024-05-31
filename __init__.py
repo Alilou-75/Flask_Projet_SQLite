d7302120b0cf2b2facbf3c33d47f019f8d33a249
Alilou-75 <165791099+Alilou-75@users.noreply.github.com>
2024-05-31 22:32:55 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -38,7 +38,8 @@ def authentification():
             return render_template('formulaire_authentification.html', error=True)
 
     return render_template('formulaire_authentification.html', error=False)
-
+# Ajout d'une route pour selectionner un client par son Numero
+# ==========================================================
 @app.route('/fiche_client/<int:post_id>')
 def Readfiche(post_id):
     conn = sqlite3.connect('database.db')
@@ -49,8 +50,33 @@ def Readfiche(post_id):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
     
-    # Ajout d'une route pour selectionner un client par son nom
+ # Ajout d'une route pour selectionner un client par son Nom
  # ==========================================================
+@app.route('/fiche_nom_client/<string:nom>')
+def Readfiche1(nom):
+
+ if not est_authentifie():
+        # Rediriger vers la page d'authentification si l'utilisateur n'est pas authentifié
+        return redirect(url_for('authentification'))
+
+  # Si l'utilisateur est authentifié
+    return "<h2>Bravo, vous êtes authentifié</h2>"
+
+@app.route('/authentification', methods=['GET', 'POST'])
+def authentification():
+    if request.method == 'POST':
+        # Vérifier les identifiants
+        if request.form['username'] == 'user' and request.form['password'] == '12345': # password à cacher par la suite
+            session['authentifie'] = True
+            # Rediriger vers la route lecture après une authentification réussie
+            return redirect(url_for('fiche_nom_client/<string:nom>'))
+        else:
+            # Afficher un message d'erreur si les identifiants sont incorrects
+            return render_template('formulaire_authentification.html', error=True)
+
+    return render_template('formulaire_authentification.html', error=False)
+
+
 @app.route('/fiche_nom_client/<string:nom>')
 def Readfiche1(nom):
     conn = sqlite3.connect('database.db')

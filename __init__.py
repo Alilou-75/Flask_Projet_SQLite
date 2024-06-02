f78bb395caf1b4821b1a58b5a6d1c05985307757
Alilou-75 <165791099+Alilou-75@users.noreply.github.com>
2024-06-02 21:59:54 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-lfrom flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session, flash
+from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session, flash
 from flask import render_template
 from flask import json
 from urllib.request import urlopen

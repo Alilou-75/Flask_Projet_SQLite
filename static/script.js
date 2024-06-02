a3769d2cace74506c504d3180ded437d4867b8dd
Alilou-75 <165791099+Alilou-75@users.noreply.github.com>
2024-06-02 16:56:08 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const deleteButtons = document.querySelectorAll('.delete-btn');
 
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const row = event.target.closest('tr');
+            row.remove();
+        });
+    });
+});

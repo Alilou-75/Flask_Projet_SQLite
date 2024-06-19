7c5b1f0bfd0417ea46df5b5a3620df5251a19f88
Alilou-75 <165791099+Alilou-75@users.noreply.github.com>
2024-06-19 20:28:18 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -131,9 +131,9 @@ def delete_client(client_id):
     close_db(db)
     return jsonify({'message': 'Client deleted successfully'}), 200
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/supprimer_client')
+def index_clients():
+    return render_template('index_clients.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

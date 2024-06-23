308d8eb3b16ba04172511f2f6a6aadf6fe3b53e4
Alilou-75 <165791099+Alilou-75@users.noreply.github.com>
2024-06-23 17:57:08 +0200

    Create create_db3.py

---
+++ create_db3.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+connection = sqlite3.connect('database3.db')
+
+with open('schema3.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('SELLAM', 'Ali', '239, avenue Gambetta, 75020 Paris'))
+cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEROUX', 'Lucas', '456, Avenue du Soleil, 31000 Toulouse'))
+cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('MARTIN', 'Amandine', '789, Rue des Érables, 69002 Lyon'))
+cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
+
+connection.commit()
+connection.close()

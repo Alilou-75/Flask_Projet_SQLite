5b724c3a58b5028ef4570fbb1b2b3f697095ba45
Alilou-75 <165791099+Alilou-75@users.noreply.github.com>
2024-06-23 18:03:24 +0200

    Update create_db3.py

--- create_db3.py
+++ create_db3.py
@@ -7,10 +7,10 @@ with open('schema3.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('SELLAM', 'Ali', '239, avenue Gambetta, 75020 Paris'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEROUX', 'Lucas', '456, Avenue du Soleil, 31000 Toulouse'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('MARTIN', 'Amandine', '789, Rue des Érables, 69002 Lyon'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
+cur.execute("INSERT INTO utilisateurs (nom, prenom, adresse) VALUES (?, ?, ?)",('SELLAM', 'Ali', '239, avenue Gambetta, 75020 Paris'))
+cur.execute("INSERT INTO utilisateurs (nom, prenom, adresse) VALUES (?, ?, ?)",('LEROUX', 'Lucas', '456, Avenue du Soleil, 31000 Toulouse'))
+cur.execute("INSERT INTO utilisateurs (nom, prenom, adresse) VALUES (?, ?, ?)",('MARTIN', 'Amandine', '789, Rue des Érables, 69002 Lyon'))
+cur.execute("INSERT INTO utilisateurs (nom, prenom, adresse) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
 
 connection.commit()
 connection.close()

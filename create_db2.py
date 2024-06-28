fa2fa35a5c005c796ace722ce05bbfa020e9bb92
Alilou-75 <165791099+Alilou-75@users.noreply.github.com>
2024-06-28 04:06:54 +0200

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -1,41 +1,25 @@
 import sqlite3
 
-    conn = sqlite3.connect('database2.db')
-    cursor = conn.cursor()
+   connection = sqlite3.connect('database2.db')
 
-   # Création de la table 'livres'
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS livres (
-    ID_livre INTEGER PRIMARY KEY AUTOINCREMENT,
-    titre TEXT,
-    auteur TEXT,
-    annee_publication INTEGER,
-    quantite INTEGER
-)
-''')
+with open('schema2.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+# Création de la table 'livres'
+cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(1, 'Emilie', 'Victor', 2024, 10))
+cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(2, 'Didier', 'Laurent', 2023, 5))
 
 # Création de la table 'utilisateurs'
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS utilisateurs (
-    ID_user INTEGER PRIMARY KEY AUTOINCREMENT,
-    nom TEXT,
-    prenom TEXT,
-    adresse TEXT,
-    Date_inscription TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
-''')
+cur.execute("INSERT INTO utilisateurs (ID_user, nom, prenom, adresse) VALUES (?, ?, ?, ?)",(1, 'SELLAM', 'Ali', '239, avenue Gambetta, 75020 Paris'))
+cur.execute("INSERT INTO utilisateurs (ID_user, nom, prenom, adresse) VALUES (?, ?, ?, ?)",(2, 'LEROUX', 'Lucas', '456, Avenue du Soleil, 31000 Toulouse'))
+cur.execute("INSERT INTO utilisateurs (ID_user, nom, prenom, adresse) VALUES (?, ?, ?, ?)",(3, 'MARTIN', 'Amandine', '789, Rue des Érables, 69002 Lyon'))
+cur.execute("INSERT INTO utilisateurs (ID_user, nom, prenom, adresse) VALUES (?, ?, ?, ?)",(4, 'LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
 
 # Création de la table 'emprunts'
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS emprunts (
-    ID_emprunt INTEGER PRIMARY KEY AUTOINCREMENT,
-    ID_user INTEGER,
-    ID_livre INTEGER,
-    date_emprunt TEXT,
-    FOREIGN KEY (ID_user) REFERENCES utilisateurs (ID_user),
-    FOREIGN KEY (ID_livre) REFERENCES livres (ID_livre)
-)
-''')
+cur.execute("INSERT INTO emprunts (ID_emprunt, ID_user, ID_livre, date_emprunt, Date_retour_prevue, Date_retour_effective ) VALUES (?, ?, ?, ?, ?, ?)",(1, 1, 1))
+
 
 conn.commit()
 conn.close()

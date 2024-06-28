0ab794af44226d99f04726a4096f240f85d50974
Alilou-75 <165791099+Alilou-75@users.noreply.github.com>
2024-06-28 03:15:25 +0200

    Update schema2.sql

--- schema2.sql
+++ schema2.sql
@@ -8,20 +8,20 @@ CREATE TABLE livres (
 );
 DROP TABLE IF EXISTS Utilisateurs;
 CREATE TABLE Utilisateurs (
-    ID_utilisateur INTEGER PRIMARY KEY,
+    ID_user INTEGER PRIMARY KEY,
     Nom VARCHAR(255),
     Prenom VARCHAR(255),
-    Email VARCHAR(255),
+    Adresse VARCHAR(255),
     Date_inscription TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 DROP TABLE IF EXISTS Emprunts;
 CREATE TABLE Emprunts (
     ID_emprunt INTEGER PRIMARY KEY,
-    ID_utilisateur INTEGER,
+    ID_user INTEGER,
     ID_livre INTEGER,
     Date_emprunt DATE,
     Date_retour_prevue DATE,
     Date_retour_effective DATE,
-    FOREIGN KEY (ID_utilisateur) REFERENCES Utilisateurs(ID_utilisateur),
+    FOREIGN KEY (ID_user) REFERENCES Utilisateurs(ID_user),
     FOREIGN KEY (ID_livre) REFERENCES Livres(ID_livre)
 );

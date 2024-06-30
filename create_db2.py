fe19742df98e17e9d0f209d39d61f96aef4342cf
Alilou-75 <165791099+Alilou-75@users.noreply.github.com>
2024-06-30 23:33:06 +0200

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -12,10 +12,10 @@ cur.execute("INSERT INTO Livres (Titre, Auteur, Annee_publication, Quantite) VAL
 cur.execute("INSERT INTO Livres (Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?)",('Didier', 'Laurent', 2023, 5))
 
 # Création de la table 'utilisateurs'
-cur.execute("INSERT INTO utilisateurs (nom, prenom, adresse) VALUES (?, ?, ?)",('SELLAM', 'Ali', '239, avenue Gambetta, 75020 Paris'))
-cur.execute("INSERT INTO utilisateurs (nom, prenom, adresse) VALUES (?, ?, ?)",('LEROUX', 'Lucas', '456, Avenue du Soleil, 31000 Toulouse'))
-cur.execute("INSERT INTO utilisateurs (nom, prenom, adresse) VALUES (?, ?, ?)",('MARTIN', 'Amandine', '789, Rue des Érables, 69002 Lyon'))
-cur.execute("INSERT INTO utilisateurs (nom, prenom, adresse) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
+cur.execute("INSERT INTO Utilisateurs (Nom, Prenom, Adresse) VALUES (?, ?, ?)",('SELLAM', 'Ali', '239, avenue Gambetta, 75020 Paris'))
+cur.execute("INSERT INTO Utilisateurs (Nom, Prenom, Adresse) VALUES (?, ?, ?)",('LEROUX', 'Lucas', '456, Avenue du Soleil, 31000 Toulouse'))
+cur.execute("INSERT INTO Utilisateurs (Nom, Prenom, Adresse) VALUES (?, ?, ?)",('MARTIN', 'Amandine', '789, Rue des Érables, 69002 Lyon'))
+cur.execute("INSERT INTO Utilisateurs (Nom, Prenom, Adresse) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
 
 # Création de la table 'emprunts'
 cur.execute("INSERT INTO emprunts (ID_emprunt, ID_user, ID_livre, date_emprunt, Date_retour_prevue, Date_retour_effective ) VALUES (?, ?, ?, ?, ?, ?)",(1, 1, 1))

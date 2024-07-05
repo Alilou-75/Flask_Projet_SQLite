4ebf3ea1da78e7409afa5db490bfd6d99df1dcef
Alilou-75 <165791099+Alilou-75@users.noreply.github.com>
2024-07-06 00:35:28 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -295,9 +295,9 @@ def retourner_livre():
     conn = sqlite3.connect('database2.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM Emprunts;')
-    livre = cursor.fetchall()
+    data = cursor.fetchall()
     conn.close()
-    return render_template('livres_empruntés.html', livre=livre)
+    return render_template('livres_empruntés.html', data=data)
 
 # Route pour gérer le processus de retour:
 #=========================================
